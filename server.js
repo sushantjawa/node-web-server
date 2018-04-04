0c9330a1d656e69f05c3d1727687dcb6d31a3fd3
jawa.sushant <jawa.sushant@gmail.com>
2018-04-04 08:27:04 -0400

    projects.hbs

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express=require('express');
 const hbs=require('hbs');
-
+const fs=require('fs');
 var app=express();
-
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine','hbs');
 app.use(express.static(__dirname + '/public'));
+app.use((req,res,next) => {
+var now=new Date().toString();
+  var log=`${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    if(err)
+      {console.log("unable to log");}
+  })
+  next();
+});
+
+// app.use((req,res,next)=>{
+// res.render('maintenance.hbs');	
+// });
 
 hbs.registerHelper('getCurrentYear',()=>{
   return new Date().getFullYear()
@@ -29,6 +42,13 @@ app.get('/',(req,res)=>
     pageMessage:"Welcome to the website"
   });
   
+});
+
+app.get('/projects',(req,res)=>
+        {
+  res.render('projects.hbs',{
+    pageTitle:'Projects'
+  });
 });
 
 app.listen(3000,()=>{
